f51fb164431c12c9714e3421ae6a26987fc4a9d7
rouadkhili <rouadkhili4@gmail.com>
2025-04-09 13:20:55 +0100

    Initial commit

---
+++ springboot-deploy/src/main/java/com/example/springboot_deploy/SpringbootDeployApplication.java
@@ -0,0 +1,16 @@
+package com.example.springboot_deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDeployApplication.class, args);
+	}
+	public String home(){
+		return "welcomehome";
+	}
+
+}
---
+++ springboot-deploy/src/test/java/com/example/springboot_deploy/springboot_deployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class springboot_deployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
